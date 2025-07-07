80fe6dd5ccc07ae593865879dbe1879c5e66edbc
inho choo <choo4r@gmail.com>
2025-07-07 15:58:45 +0900

    [UT] WriteExceedIndex

--- TestShell/test_with_mock.cpp
+++ TestShell/test_with_mock.cpp
@@ -11,3 +11,12 @@ public:
 	MOCK_METHOD(string, write, (int, int), (override));
 };
 
+TEST(SSD, WriteExccedIndex) {
+	MockStroage mock;
+
+	string expected = "ERROR";
+	EXPECT_CALL(mock, write(Gt(99), _))
+		.WillRepeatedly(Return("ERROR"));
+
+	EXPECT_EQ(string(expected), string(mock.write(100, 0xFFFF)));
+}
