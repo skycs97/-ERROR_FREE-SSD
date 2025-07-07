d6a70f88e528bdd88833eb0ee092f67a0dae5c3d
skycs97 <skycs97@gmail.com>
2025-07-07 16:10:18 +0900

    [test] Add read test with mock

--- TestShell/test_with_mock.cpp
+++ TestShell/test_with_mock.cpp
@@ -11,3 +11,24 @@ public:
 	MOCK_METHOD(string, write, (int, int), (override));
 };
 
+
+
+TEST(TestShell, ReadMock) {
+	MockStroage storage;
+	EXPECT_CALL(storage, read)
+		.WillOnce(Return("0x12341234"));
+
+	string actual = storage.read(30);
+
+	EXPECT_EQ("0x12341234", actual);
+}
+
+TEST(TestShell, ReadFailMock) {
+	MockStroage storage;
+	EXPECT_CALL(storage, read)
+		.WillOnce(Return("Error"));
+
+	string actual = storage.read(100);
+
+	EXPECT_EQ("Error", actual);
+}
