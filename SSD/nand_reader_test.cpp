aa7ba4f0f6b9352e4084b1d09d0ad0c61df127f7
MJ <julysky40@gmail.com>
2025-07-07 16:07:28 +0900

    [refactor] add test fixture in nand_reader_test.cpp

--- SSD/nand_reader_test.cpp
+++ SSD/nand_reader_test.cpp
@@ -3,22 +3,24 @@
 #include "nand_flash_memory_mock.h"
 
 using namespace testing;
+static constexpr int LBA_IN_RANGE = 10;
+static constexpr int LBA_OUT_RANGE = 105;
+static constexpr int NAND_WRITTEN_VALUE = 0x12345678;
 
-TEST(NandReader, readTestWithSuccessResult) {
+class NandReaderFixture : public Test {
+public:
 	NandFlashMemoryMock memory;
-	NandReader nand_reader(&memory);
-	EXPECT_CALL(memory, read(_)).WillRepeatedly(Return(0x12345678));
-	int lba = 10;
-	int actual = nand_reader.read(lba);
-	int expected = 0x12345678;
+	NandReader nand_reader{ &memory };
+};
+
+TEST_F(NandReaderFixture, readTestWithSuccessResult) {
+	EXPECT_CALL(memory, read(_)).WillRepeatedly(Return(NAND_WRITTEN_VALUE));
+	int actual = nand_reader.read(LBA_IN_RANGE);
+	int expected = NAND_WRITTEN_VALUE;
+
 	EXPECT_EQ(expected, actual);
 };
 
-TEST(NandReader, readTestWithOutRangedLBA) {
-	NandFlashMemoryMock memory;
-	NandReader nand_reader(&memory);
-	EXPECT_CALL(memory, read(_)).WillRepeatedly(Return(0x12345678));
-	int lba = 105;
-
-	EXPECT_THROW(nand_reader.read(lba), std::runtime_error);
+TEST_F(NandReaderFixture, readTestWithOutRangedLBA) {
+	EXPECT_THROW(nand_reader.read(LBA_OUT_RANGE), std::runtime_error);
 };
