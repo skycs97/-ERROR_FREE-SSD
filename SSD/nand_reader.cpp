ceea59f9313d89d1b103c70b802c10bc5de63310
MJ <julysky40@gmail.com>
2025-07-07 16:31:49 +0900

    [refactor] modify error message

--- SSD/nand_reader.cpp
+++ SSD/nand_reader.cpp
@@ -2,6 +2,6 @@
 #include <stdexcept>
 string NandReader::read(int LBA)
 {
-	if (LBA < 0 || LBA >= 100) throw std::range_error("LBA 값은 0과 99 사이의 값이어야 합니다.\n");
+	if (LBA < 0 || LBA >= 100) throw std::range_error("The LBA must be a value between 0 and 99.\n");
 	return nandFlashMemory->read(LBA);
 }
