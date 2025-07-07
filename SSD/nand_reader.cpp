54dc9903e6775e14d23034ce52ee9508cc80f0fa
MJ <julysky40@gmail.com>
2025-07-07 15:46:20 +0900

    [feat] implement logic of  nand read with out langed LBA

--- SSD/nand_reader.cpp
+++ SSD/nand_reader.cpp
@@ -1,6 +1,7 @@
 #include "nand_reader.h"
-
+#include <stdexcept>
 int NandReader::read(int LBA)
 {
+	if (LBA < 0 || LBA >= 100) throw std::range_error("LBA 값은 0과 99 사이의 값이어야 합니다.\n");
 	return nandFlashMemory->read(LBA);
 }
