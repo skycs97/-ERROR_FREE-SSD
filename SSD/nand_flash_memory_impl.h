3560512d222a0da46cbad3cb888f3d6fbf7dccff
MJ <julysky40@gmail.com>
2025-07-08 08:46:23 +0900

    [test] add read test of NandFlashMemoryImpl

--- SSD/nand_flash_memory_impl.h
+++ SSD/nand_flash_memory_impl.h
@@ -1,10 +1,9 @@
 #pragma once
 #include "nand_flash_memory.h"
 
-class NandFlashMemoryImpl : public NandFlashMemory
-{
+class NandFlashMemoryImpl : public NandFlashMemory {
 public:
 	NandFlashMemoryImpl() = default;
-	string read(int LBA) override { return ""; }
-	string write(int lba, int data) override { return ""; }
+	string read(int LBA);
+	string write(int lba, int data);
 };
