1c88742a3970a77747d35778941580417b22e494
skycs97 <skycs97@gmail.com>
2025-07-07 15:48:23 +0900

    [fix] 잘못된 변수명 수정

--- TestShell/test_with_mock.cpp
+++ TestShell/test_with_mock.cpp
@@ -1,12 +1,13 @@
 #include "gmock/gmock.h"
-#include "stroage.h"
+#include "storage.h"
 #include <string>
 
 using std::string;
 using namespace testing;
 
-class MockStroage : public Stroage {
+class MockStroage : public Storage {
+public:
 	MOCK_METHOD(string, read, (int), (override));
-	MOCK_METHOD(string, write, (int, int), (override);
+	MOCK_METHOD(string, write, (int, int), (override));
 };
 
