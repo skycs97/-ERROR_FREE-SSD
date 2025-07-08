dd1489d92aa46080eb753f4917b715e25e8d02bc
soonwonchoi <soonwon.choi@samsung.com>
2025-07-08 13:10:46 +0900

    [GREEN] Writer 구현 1. write 매개변수 변경 2. 전체 read 후 write 하도록 수정

--- SSD/nand_writer.cpp
+++ SSD/nand_writer.cpp
@@ -2,6 +2,9 @@
 
 string NandWriter::write(int lba, string data)
 {
-	vector<string> datas;
+	if (lba < 0 || lba > 99) return "ERROR";
+
+	vector<string> datas = nandFlashMemory->read();
+	datas[lba] = data;
 	return nandFlashMemory->write(datas);
 }
