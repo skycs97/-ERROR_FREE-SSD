dc5263b183a77a54a0a7a6d54be10e8be33f3d1b
dongig.shin <dongig.shin@samsung.com>
2025-07-08 14:01:28 +0900

    [TEST] add TC for command class

---
+++ TestShell/test_command.cpp
@@ -0,0 +1,58 @@
+#include "gmock/gmock.h"
+#include <string>
+#include <vector>
+#include "command.h"
+
+class CommandFactoryTest : public testing::Test {
+public:
+	FactoryCommand fc;
+
+	Command* getCommand(const std::string& cmd) { return fc.makeCommand(cmd); }
+};
+TEST_F(CommandFactoryTest, Read) {
+	
+	Command* command = getCommand("read 0");
+
+	EXPECT_TRUE(command->getNumOfArgs() == 2);
+	EXPECT_EQ("read", command->getShellCommands().front());
+}
+
+TEST_F(CommandFactoryTest, Write) {
+
+	Command* command = getCommand("write 0 1");
+
+	EXPECT_TRUE(command->getNumOfArgs() == 3);
+	EXPECT_EQ("write", command->getShellCommands().front());
+}
+
+TEST_F(CommandFactoryTest, exit) {
+
+	Command* command = getCommand("exit");
+
+	EXPECT_TRUE(command->getNumOfArgs() == 1);
+	EXPECT_EQ("exit", command->getShellCommands().front());
+}
+
+TEST_F(CommandFactoryTest, help) {
+
+	Command* command = getCommand("help");
+
+	EXPECT_TRUE(command->getNumOfArgs() == 1);
+	EXPECT_EQ("help", command->getShellCommands().front());
+}
+
+TEST_F(CommandFactoryTest, fullwrite) {
+
+	Command* command = getCommand("fullwrite");
+
+	EXPECT_TRUE(command->getNumOfArgs() == 1);
+	EXPECT_EQ("fullwrite", command->getShellCommands().front());
+}
+
+TEST_F(CommandFactoryTest, fullread) {
+
+	Command* command = getCommand("fullread");
+
+	EXPECT_TRUE(command->getNumOfArgs() == 1);
+	EXPECT_EQ("fullread", command->getShellCommands().front());
+}
