724e6d98f8cc5f24cd18ee1e14496cc95ca6315b
soonwonchoi <soonwon.choi@samsung.com>
2025-07-08 13:38:19 +0900

    [Fix] Out of range 일 경우, Exception 발생하도록 수정

--- SSD/nand_writer.cpp
+++ SSD/nand_writer.cpp
@@ -1,8 +1,9 @@
 #include "nand_writer.h"
+#include <stdexcept>
 
 string NandWriter::write(int lba, string data)
 {
-	if (isOutOfRange(lba)) return "ERROR";
+	if (isOutOfRange(lba)) throw std::range_error("The LBA must be a value between 0 and 99.\n");
 
 	vector<string> datas = nandFlashMemory->read();
 	datas[lba] = data;
