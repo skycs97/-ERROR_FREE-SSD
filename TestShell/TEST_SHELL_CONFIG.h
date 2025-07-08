bcb956059a9147a534f89b05b87092d8947fa015
skycs97 <skycs97@gmail.com>
2025-07-08 14:27:20 +0900

    [feat] help command 구현 및 CONFIG 파일 생성

---
+++ TestShell/TEST_SHELL_CONFIG.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+using std::string;
+using std::vector;
+
+const vector<string> DEVELOPERS = {
+	"skycs97",
+	"choo4r",
+	"dongs83",
+	"iamsoonwonchoi",
+	"JIeunAmy",
+	"julysky123",
+	"zeroexit-ryu"
+};
+
+const vector<string> AVAILABLE_COMMAND_LIST = {
+	"help",
+	"read",
+	"write",
+	"exit",
+	"fullwrite",
+	"fullread"
+};
