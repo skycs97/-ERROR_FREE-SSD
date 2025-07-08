233b940feec9e15df0be5b7d73ee3d19cbfd824b
soonwonchoi <soonwon.choi@samsung.com>
2025-07-08 13:10:46 +0900

    [GREEN] Writer 구현 1. write 매개변수 변경 2. 전체 read 후 write 하도록 수정

--- SSD/nand_writer_test.cpp
+++ SSD/nand_writer_test.cpp
@@ -4,18 +4,26 @@
 
 using namespace testing;
 
+static const int MAX_LBA = 100;
 static const int VALID_RANGE_LBA = 53;
 static const int INVALID_RANGE_LBA = 999;
 static const string VALID_DATA = "0xAAAABBBB";
+static const string INIT_DATA = "0x00000000";
 
 TEST(Writer, ValidCase)
 {
 	// arrange
 	NandFlashMemoryMock nand;
 	NandWriter writer(&nand);
+	vector<string> dummy_data(MAX_LBA, INIT_DATA);
 
 	// act, assert
+	EXPECT_CALL(nand, read())
+		.Times(1)
+		.WillOnce(Return(dummy_data));
+
 	EXPECT_CALL(nand, write(_))
+		.Times(1)
 		.WillRepeatedly(Return(""));
 
 	string expected = "";
