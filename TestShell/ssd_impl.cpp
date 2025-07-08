507bcd4db349b60c19f65b6e995168fe433a6cbb
skycs97 <skycs97@gmail.com>
2025-07-09 08:41:16 +0900

    [feat] SSD impl 기본 구현

---
+++ TestShell/ssd_impl.cpp
@@ -0,0 +1,42 @@
+#include "ssd_impl.h"
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
+string SsdImpl::read(const string& index)
+{
+	string cmd = ssdExcutable + " R " + index;
+
+	int processRet = system(cmd.c_str());
+
+    // 읽기 모드로 파일 열기
+    std::ifstream inputFile(ssdOutputPath);
+    std::string line;
+
+    if (inputFile.is_open() == false) {
+        return "Error";
+    }
+    getline(inputFile, line);
+    inputFile.close();
+
+    return line;
+}
+
+string SsdImpl::write(const string& index, const string& data)
+{
+    string cmd = ssdExcutable + " W " + index + " " + data;
+
+    int processRet = system(cmd.c_str());
+
+    // 읽기 모드로 파일 열기
+    std::ifstream inputFile(ssdOutputPath);
+    std::string line;
+
+    if (inputFile.is_open() == false) {
+        return "Error";
+    }
+    getline(inputFile, line);
+    inputFile.close();
+
+    return line;
+}
