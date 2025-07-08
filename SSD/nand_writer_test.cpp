f1edbaba944be2966ad2fba38b346881ff304151
soonwonchoi <soonwon.choi@samsung.com>
2025-07-08 15:35:07 +0900

    [Fix] LBA OOR 검사 Parser에서 수행함에 따라, Reader/Writer의 OOR Check TC 제거

--- SSD/nand_writer_test.cpp
+++ SSD/nand_writer_test.cpp
@@ -5,7 +5,6 @@
 using namespace testing;
 
 static const int VALID_RANGE_LBA = 53;
-static const int INVALID_RANGE_LBA = 999;
 static const string VALID_DATA = "0xAAAABBBB";
 static const string INIT_DATA = "0x00000000";
 
@@ -31,13 +30,4 @@ TEST_F(WriterFixture, ValidCase)
 	string expected = "";
 	string actual = writer.write(VALID_RANGE_LBA, VALID_DATA);
 	EXPECT_EQ(expected, actual);
-}
-
-TEST_F(WriterFixture, InvalidCase_OutOfRange)
-{
-	// act, assert
-	EXPECT_CALL(nand, write(_))
-		.Times(0);
-
-	EXPECT_THROW(writer.write(INVALID_RANGE_LBA, VALID_DATA), std::runtime_error);
 }
