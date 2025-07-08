5239cbf459ea80af247c557faad73e62f1a30f0a
soonwonchoi <soonwon.choi@samsung.com>
2025-07-08 14:58:05 +0900

    [Refactoring] LBA out of range에 대하여 Parser에서 한 번 검사

--- SSD/nand_reader.cpp
+++ SSD/nand_reader.cpp
@@ -1,9 +1,7 @@
 #include "nand_reader.h"
-#include "global_config.h"
-#include <stdexcept>
+
 string NandReader::read(int LBA)
 {
-	if (LBA < MIN_LBA || LBA > MAX_LBA) throw std::range_error("The LBA must be a value between 0 and 99.\n");
 	vector<string> datas = nandFlashMemory->read();
 	return datas.at(LBA);
 }
