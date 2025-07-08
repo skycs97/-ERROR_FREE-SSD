387710ae45139c27505760c46f69f478f6fea555
YoungJae Ryu <zeroexit@gmail.com>
2025-07-08 10:29:51 +0900

    [RED][GREEN] add argument parser Exception TC

--- SSD/ArgumentParser_test.cpp
+++ SSD/ArgumentParser_test.cpp
@@ -15,3 +15,54 @@ TEST(ArgumentParserTest, ReadCommandTest) {
 	EXPECT_EQ(ArgumentParser::READ_CMD, parser.getCmdType());
 	EXPECT_EQ(10, parser.getAddr());
 }
+
+TEST(ArgumentParserTest, ReadCommandExceptionTest) {
+	ArgumentParser parser;
+	const char* argv[] = {
+		"ssd.exe",
+		"R",
+		"1000"
+	};	
+
+	EXPECT_THROW(parser.parse_args(3, argv), std::invalid_argument);
+}
+
+TEST(ArgumentParserTest, WriteCommandTest) {
+	ArgumentParser parser;
+	const char* argv[] = {
+		"ssd.exe",
+		"W",
+		"20",
+		"0xAAAABBBB"
+	};
+
+	parser.parse_args(4, argv);
+
+	EXPECT_EQ(ArgumentParser::WRITE_CMD, parser.getCmdType());
+	EXPECT_EQ(20, parser.getAddr());
+	EXPECT_EQ(0xAAAABBBB, parser.getData());
+}
+
+TEST(ArgumentParserTest, WriteCommandExceptionTest) {
+	ArgumentParser parser;
+	const char* argv[] = {
+		"ssd.exe",
+		"W",
+		"200",
+		"0xAAAABBBB"
+	};
+
+	EXPECT_THROW(parser.parse_args(4, argv), std::invalid_argument);
+}
+
+TEST(ArgumentParserTest, WriteCommandDataExceptionTest) {
+	ArgumentParser parser;
+	const char* argv[] = {
+		"ssd.exe",
+		"W",
+		"20",
+		"0xRRRRQQQQQ"
+	};
+
+	EXPECT_THROW(parser.parse_args(4, argv), std::invalid_argument);
+}
