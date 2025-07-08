1a74b9dd90a7246f8da08fddb224d64742b50787
soonwonchoi <soonwon.choi@samsung.com>
2025-07-08 15:45:24 +0900

    [Refactoring] NandFlashMemoryImplFixture 도입

--- SSD/nand_flash_memory_impl_test.cpp
+++ SSD/nand_flash_memory_impl_test.cpp
@@ -7,10 +7,33 @@
 using namespace testing;
 using std::vector;
 
-TEST(NandFlashMemoryImpl, readTest) {
+class NandFlashMemoryImplFixture : public Test {
+public:
+	vector<string> GetFullDataWithLBA()
+	{
+		vector<string> expectingWriteDatas;
+		for (int i = MIN_LBA; i <= MAX_LBA; i++) {
+			std::ostringstream oss;
+			oss << i << '\t' << "0x00001111";
+			expectingWriteDatas.push_back(oss.str());
+		}
+		return expectingWriteDatas;
+	}
+
+	vector<string> GetFullDataWihtoutLBA()
+	{
+		vector<string> writingDatas;
+		for (int i = MIN_LBA; i <= MAX_LBA; i++) {
+			writingDatas.push_back("0x00001111");
+		}
+		return writingDatas;
+	}
+
 	FileHandlerMock mockedFileHandler;
 	NandFlashMemoryImpl memory{ &mockedFileHandler };
+};
 
+TEST_F(NandFlashMemoryImplFixture, readTest) {
 	vector<string> writtenDatas = { "0\t0x00000000","1\t0x00000001" };
 	EXPECT_CALL(mockedFileHandler, read("ssd_nand.txt")).WillRepeatedly(Return(writtenDatas));
 	vector<string> expected = { "0x00000000", "0x00000001" };
@@ -19,20 +42,10 @@ TEST(NandFlashMemoryImpl, readTest) {
 	EXPECT_EQ(expected, actual);
 }
 
-TEST(NandFlashMemoryImpl, writeTest) {
-	FileHandlerMock mockedFileHandler;
-	NandFlashMemoryImpl memory{ &mockedFileHandler };
-
-	vector<string> expectingWriteDatas;
-	for (int i = 0; i < 100; i++) {
-		std::ostringstream oss;
-		oss << i << '\t' << "0x00001111";
-		expectingWriteDatas.push_back(oss.str());
-	}
-	vector<string> writingDatas;
-	for (int i = 0; i < 100; i++) {
-		writingDatas.push_back("0x00001111");
-	}
+TEST_F(NandFlashMemoryImplFixture, writeTest) {
+	vector<string> expectingWriteDatas = GetFullDataWithLBA();
+	vector<string> writingDatas = GetFullDataWihtoutLBA();
+	
 	EXPECT_CALL(mockedFileHandler, write("ssd_nand.txt", expectingWriteDatas)).Times(1);
 	string actual = memory.write(writingDatas);
 
