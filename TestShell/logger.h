cf96d2523260e332b3f5b54f2a8ef0477b1a4871
dongig.shin <dongig.shin@samsung.com>
2025-07-09 12:38:51 +0900

    [feat] add logger function

---
+++ TestShell/logger.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#define logger Logger::getInstance()
+
+namespace {
+	const std::string LOGFILE_PREFIX = "until_";
+	const std::string LOGFILE_EXTENSION = ".log";
+	const std::string LOGFILE_ZIP_EXTENSION = ".zip";
+	const std::string LOGFILE_NAME = "latest" + LOGFILE_EXTENSION;
+	const int LOGFILE_MAXSIZE = 10 * 1024;
+	const std::string LOGFILE_SEARCH_PATTERN = LOGFILE_PREFIX + "*" + LOGFILE_EXTENSION;
+}
+
+class Logger {
+public:
+	static Logger& getInstance() {
+		static Logger instance;
+		return instance;
+	}
+
+	void print(const std::string& functionName, const std::string& message);
+	std::size_t getLogFileSize();
+
+private:
+	Logger();
+	~Logger();
+
+	Logger(const Logger&) = delete;
+	Logger& operator=(const Logger&) = delete;
+
+	std::ofstream logFile;
+	const std::string logFileName = LOGFILE_NAME;
+	const std::size_t maxFileSize = LOGFILE_MAXSIZE;
+
+	void openLogFile();
+	std::string getCurrentTimestamp();
+	std::string getFileTimeForName();
+	void renamePreviousLogFiles();
+	void checkRenameFIles();
+};
