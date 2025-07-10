6a096a84234b947433be73e830166ca6e0512987
soonwonchoi <soonwon.choi@samsung.com>
2025-07-10 13:00:04 +0900

    [Refactoring][FileHandler] FileHandler Interface 추가 + FileHandlerImpl&FileHandlerMock이 각각 상속

--- SSD/file_handler.h
+++ SSD/file_handler.h
@@ -1,39 +1,32 @@
 #pragma once
-#include <string>
-#include <cstring>
-#include <Windows.h>
-#include <stdexcept>
-#include <fstream>
 #include <vector>
-#include "global_config.h"
+#include <string>
 
-using std::ofstream;
 using std::string;
 using std::vector;
 
 class FileHandler {
 public:
-	FileHandler() = default;
-	
-	// Path에 있는 File Read 하여 포인터 반환
-	virtual char* readFile(const string& path);
-
 	// Path에 Directory 생성
-	void createDirectory(const string& path);
-	
+	virtual void createDirectory(const string& path) = 0;
+
 	// Path에 Empty File 생성
-	virtual void createFile(const string& path);
+	virtual void createFile(const string& path) = 0;
+
+	// Path에 있는 File Read 하여 포인터 반환
+	virtual char* readFile(const string& path) = 0;
 	
 	// Path에 있는 파일에 Data 쓰기
-	virtual void writeData(const string& path, const string& data);
-	
-	virtual bool isFileExistByMatchLength(const string& dir_path, const string& file_name, int len);
-	
+	virtual void writeData(const string& path, const string& data) = 0;
+
 	// old_name의 파일 이름 변경
-	virtual void rename(const string& old_name, const string& new_name) const;
+	virtual void rename(const string& old_name, const string& new_name) const = 0;
+	
+	// Directory에서 file 이름의 len 만큼만 비교하여 같은 파일이 존재하는지 여부 반환
+	virtual bool isFileExistByMatchLength(const string& dir_path, const string& file_name, int len) = 0;
 	
 	// Path에 있는 파일들 중에서 prefix가 포함되어 있는 파일명 반환
-	virtual vector<string> getFileUsingPrefix(const string& path, const string& prefix);
+	virtual vector<string> getFileUsingPrefix(const string& path, const string& prefix) = 0;
 
 private:
 
--- SSD/file_handler_mock.h
+++ SSD/file_handler_mock.h
@@ -4,12 +4,11 @@
 
 class FileHandlerMock : public FileHandler {
 public:
+	MOCK_METHOD(void, createDirectory, (const string&), (override));
 	MOCK_METHOD(void, createFile, (const string&), (override));
-	MOCK_METHOD(void, writeData, (const string&, const string&), (override));
 	MOCK_METHOD(char*, readFile, (const string&), (override));
-	MOCK_METHOD(vector<string>, read, (const string&), (override));
-	MOCK_METHOD(bool, isFileExistByMatchLength, (const string&, const string&, int), (override));
+	MOCK_METHOD(void, writeData, (const string&, const string&), (override));
 	MOCK_METHOD(void, rename, (const string&, const string&), (override, const));
+	MOCK_METHOD(bool, isFileExistByMatchLength, (const string&, const string&, int), (override));
 	MOCK_METHOD(vector<string>, getFileUsingPrefix, (const string&, const string&), (override));
-
 };
